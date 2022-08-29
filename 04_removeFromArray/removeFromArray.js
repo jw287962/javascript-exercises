b56850a93fd42bd38d57aef3c23c5d51ee3d7c48
JasonPC <jasonwong287962@gmail.com>
2022-08-29 14:00:37 -0500

    pass on all test for 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove1, remove2,remove3,remove4) {
+    const newArray = new Array();
 
+loop1:
+    for(const things of array){
+        
+        if(things === remove1 || things === remove2 || things === remove3 || things === remove4){
+          continue loop1;
+           
+        }
+      
+    newArray.push(things)
+}
+return newArray;
 };
 
 // Do not edit below this line
