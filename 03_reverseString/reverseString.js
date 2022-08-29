255a2d3baf2e35b9c0b4c8bfbc5c1868b769bbee
JasonPC <jasonwong287962@gmail.com>
2022-08-29 13:21:32 -0500

    pass on all test for reverseString with for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+const words = string.split('');
+let reversed = '';
+for(let i = words.length-1; i >=0; i--){
+    reversed += words[i];
 
+}
+return reversed;
 };
 
 // Do not edit below this line
