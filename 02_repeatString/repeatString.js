0c6f56308e4c84fe5054f2d43aa7ba882c94507d
JasonPC <jasonwong287962@gmail.com>
2022-08-29 12:18:10 -0500

    pass all tests for repeatString 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+   let i = 0;
+   let holder = ""; 
+   if(num < 0) {
+      return 'ERROR';
+  }
+   if(!num && num !==0){
+   num = number;
+}
+   while(i < num){
+    
+        holder += string
+        i++
+   }
+   
 
+   return holder;
 };
 
 // Do not edit below this line
