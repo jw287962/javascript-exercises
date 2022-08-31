585da23b2a103525c8b97568332a7608091bc7ae
JasonPC <jasonwong287962@gmail.com>
2022-08-31 16:18:21 -0500

    pass all test on sumAll (5)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    
+
+    if(firstNum > secondNum){
+        let holder = firstNum;
+        firstNum = secondNum;
+        secondNum = holder;
+    }
+ 
+    if(firstNum < 0 || secondNum < 0) return "ERROR";
+
+    if (typeof firstNum === 'number' && typeof secondNum === 'number'){
+        for (let i = firstNum; i<= secondNum; i++){
+        sum+=i;
+        }
+    return sum;
+}
+else {
+    return "ERROR";
+}
 
 };
 
