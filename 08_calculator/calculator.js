879cdc522f30de3bcf4706c4391449ff21d88fea
JasonPC <jasonwong287962@gmail.com>
2022-08-31 18:04:15 -0500

    pass on all test for calcultor (08)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let holder = 0;
+	for(const num of array){
+    holder += num;
+  }
+  return holder;
+}
+
+const multiply = function(array1) {
+  let holder = array1[0];;
+  let total;
+	for(let i = 1; i <array1.length; i++){
+     
+    total = holder*array1[i];
+    holder = total;
+  }
+  return total;
 };
 
-const multiply = function() {
+const power = function(num1, num2) {
+  let total = num1;
+	for(let i = 1; i <num2; i++){
+    total= total* num1;
 
+  }
+  return total;
 };
 
-const power = function() {
-	
-};
+const factorial = function(num1) {
+  let fact = num1;
+  
 
-const factorial = function() {
-	
+  for (let i = num1; i>1; i--)
+  {
+     fact = fact*(i-1)
+  }
+  if(num1 === 0) return 1;
+  else 
+	return fact;
+
 };
 
 // Do not edit below this line
