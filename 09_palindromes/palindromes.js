d77ca167fc0c6821604c7ebf0dd55e923acadb64
JasonPC <jasonwong287962@gmail.com>
2022-09-02 23:06:27 -0500

    passed all palindromes.js (09)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let textRemovePunct = text.toLowerCase()
+     textRemovePunct = textRemovePunct.replaceAll(/[^a-z]/g,'');
 
+let arrayHolder = textRemovePunct.split('');
+
+let reverseTextHolder = '';
+for(let i = (arrayHolder.length -1); i >=0; i--){
+  reverseTextHolder += (arrayHolder[i]);
+  
+}
+
+
+
+
+if(reverseTextHolder.toLowerCase() === textRemovePunct.toLowerCase()){
+    return true;
+    
+}
+else return false;
 };
 
 // Do not edit below this line
