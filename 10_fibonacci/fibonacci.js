043d345f1025ffe4035b36f45894102b5b07e02f
JasonPC <jasonwong287962@gmail.com>
2022-09-04 00:07:30 -0500

    pass all test on fibonacci #10 problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciNumber) {
+ let firstNumber = 1;
+ let secondNumber = 1;
+ let total = 1;
+ if(fibonacciNumber < 0) {
+    return 'OOPS';
+}
+ for(let i = 3;i <=fibonacciNumber; i++){
+    if (fibonacciNumber> 2){
+        total = firstNumber + secondNumber; 
+        firstNumber = secondNumber;
+        secondNumber = total;
+    }
+        
+        else return 1;
+ }
+ return total;
 
 };
 
