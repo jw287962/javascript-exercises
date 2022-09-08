cf2d1e3e1542b67bc7170b00c408da533c242f7a
JasonPC <jasonwong287962@gmail.com>
2022-09-08 15:01:44 -0500

    findtheoldest pass on all test (12)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentOldest = 0;
+    let i = 0;
+    let oldest = 0;
+
+    let currentYear = new Date().getFullYear() 
+ let alive = false;
+    for(const person of people){
+        if(person.yearOfDeath != null){
+         
+
+
+  let currentAge =  person.yearOfDeath - person.yearOfBirth;
+  if(currentOldest < currentAge){
+  currentOldest = currentAge;
+  oldest = i;
+  }
+  i++;
+}
+else {
+    let currentAge =   currentYear- person.yearOfBirth; //current age of the living person
+    if(currentOldest <currentAge){
+        currentOldest = currentAge;
+        oldest = i;
+    }
+  
+    i++;
+}
+        }
+return people[oldest];
+
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
